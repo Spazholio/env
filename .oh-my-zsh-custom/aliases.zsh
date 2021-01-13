2f148abc653b1a3084d535945768a6efae2f9b5c
Paul Martin <github@lexal.net>
2021-01-13 13:15:20 -0500

    Missed a few the first time around

---
+++ .oh-my-zsh-custom/aliases.zsh
@@ -0,0 +1,4 @@
+alias lla='ls -Al'
+alias mountt='mount | column -t'
+alias weather='curl wttr.in'
+alias colortable='ruby -e "$(curl -fsSL https://raw.githubusercontent.com/gawin/bash-colors-256/master/colors)"'
---
+++ .oh-my-zsh-custom/functions.zsh
@@ -0,0 +1,16 @@
+# Functions
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+rf() {
+    RG_PREFIX="rga --files-with-matches"
+    local file
+    file="$(
+        FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
+            fzf --sort --preview="[[ ! -z {} ]] && rga --pretty --context 5 {q} {}" \
+                --phony -q "$1" \
+                --bind "change:reload:$RG_PREFIX {q}" \
+                --preview-window="70%:wrap"
+    )" &&
+    echo "opening $file" &&
+    open "$file"
+}
