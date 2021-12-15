107eee477d91bcc530415cf192c3112abe50198e
Paul Martin <github@lexal.net>
2021-12-15 10:30:21 -0500

    vi to vim alias

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -1,4 +1,5 @@
 alias lla='ls -Al'
 alias mountt='mount | column -t'
 alias weather='curl wttr.in'
+alias vi='vim'
 alias colortable='ruby -e "$(curl -fsSL https://raw.githubusercontent.com/gawin/bash-colors-256/master/colors)"'
